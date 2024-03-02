40903ecf2c174e1225a7e5788ce4effeb43bf152
Pavel Forinnyy <forinnyy@gmail.com>
2024-03-02 22:01:35 +0400

    Arguments support added.

--- src/ru/forinnyy/tm/Application.java
+++ src/ru/forinnyy/tm/Application.java
@@ -1,7 +1,57 @@
 package ru.forinnyy.tm;
 
+import ru.forinnyy.tm.constant.ArgumentConst;
+
 public class Application {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        processArguments(args);
     }
+
+    public static void processArguments(final String[] args) {
+        if (args == null || args.length < 1) {
+            showError();
+            return;
+        }
+        processArgument(args[0]);
+    }
+
+    public static void showError() {
+        System.out.println("[ERROR]");
+        System.err.println("The program arguments are not correct...");
+    }
+
+    public static void processArgument(final String arg) {
+        switch (arg) {
+            case ArgumentConst.VERSION:
+                showVersion();
+                break;
+            case ArgumentConst.ABOUT:
+                showAbout();
+                break;
+            case ArgumentConst.HELP:
+                showHelp();
+                break;
+            default:
+                showError();
+        }
+    }
+
+    public static void showVersion() {
+        System.out.println("[VERSION]");
+        System.out.println("1.2.0");
+    }
+
+    public static void showAbout() {
+        System.out.println("[ABOUT]");
+        System.out.println("Name: Pavel Forinnyy");
+        System.out.println("E-mail: forinnyy@gmail.com");
+    }
+
+    public static void showHelp() {
+        System.out.println("[HELP]");
+        System.out.printf("%s - Show version info.\n", ArgumentConst.VERSION);
+        System.out.printf("%s - Show developer info.\n", ArgumentConst.ABOUT);
+        System.out.printf("%s - Show command list.\n", ArgumentConst.HELP);
+    }
+
 }
