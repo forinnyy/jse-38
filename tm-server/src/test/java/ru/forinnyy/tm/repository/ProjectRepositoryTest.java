a91ae46c6564b03607a62d2c5fb32ca41f15aaa8
Pavel Forinnyy <forinnyy@gmail.com>
2025-06-27 16:00:22 +0400

    Server tests.

--- tm-server/src/test/java/ru/forinnyy/tm/repository/ProjectRepositoryTest.java
+++ tm-server/src/test/java/ru/forinnyy/tm/repository/ProjectRepositoryTest.java
@@ -1,8 +1,10 @@
 package ru.forinnyy.tm.repository;
 
 
+import lombok.NonNull;
 import lombok.SneakyThrows;
 import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
 import ru.forinnyy.tm.enumerated.Sort;
 import ru.forinnyy.tm.model.Project;
@@ -10,6 +12,7 @@ import ru.forinnyy.tm.model.Project;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
+import java.util.UUID;
 
 public final class ProjectRepositoryTest extends AbstractUserOwnedRepositoryTest<Project> {
 
@@ -20,32 +23,40 @@ public final class ProjectRepositoryTest extends AbstractUserOwnedRepositoryTest
 
     @Override
     protected Project createModel() {
-        return new Project();
+        @NonNull final Project project = new Project();
+        project.setUserId(UUID1);
+        return project;
+    }
+
+    public ProjectRepository getProjectRepository() {
+        return (ProjectRepository) createRepository();
     }
 
     @Test
     @SneakyThrows
     public void testCreateWithName() {
-        Project project = projectRepository.create("1", "Project");
+        @NonNull final Project project = getProjectRepository().create(UUID1, STRING);
+        repository.add(project);
         Assert.assertNotNull(project);
-        Assert.assertEquals(project, projectRepository.findOneById(project.getId()));
+        Assert.assertEquals(project, repository.findOneById(project.getId()));
 
-        Assert.assertThrows(NullPointerException.class, () -> projectRepository.create("1", null));
-        Assert.assertThrows(NullPointerException.class, () -> projectRepository.create(null, "Project"));
+        Assert.assertThrows(NullPointerException.class, () -> getProjectRepository().create(UUID1, null));
+        Assert.assertThrows(NullPointerException.class, () -> getProjectRepository().create(null, STRING));
     }
 
     @Test
     @SneakyThrows
     public void testCreateWithNameAndDescription() {
-        Project project = projectRepository.create("1", "Project", "Description");
+        @NonNull final Project project = getProjectRepository().create(UUID1, STRING, STRING);
+        repository.add(project);
         Assert.assertNotNull(project);
-        Assert.assertEquals(project, projectRepository.findOneById(project.getId()));
+        Assert.assertEquals(project, repository.findOneById(project.getId()));
         Assert.assertThrows(NullPointerException.class, () ->
-                projectRepository.create("1", null, "Description"));
+                getProjectRepository().create(UUID1, null, STRING));
         Assert.assertThrows(NullPointerException.class, () ->
-                projectRepository.create("1", "Project", null));
+                getProjectRepository().create(UUID1, STRING, null));
         Assert.assertThrows(NullPointerException.class, () ->
-                projectRepository.create(null, "Project", "Description"));
+                getProjectRepository().create(null, STRING, STRING));
     }
 
     @Test
@@ -72,18 +83,18 @@ public final class ProjectRepositoryTest extends AbstractUserOwnedRepositoryTest
     public void testFindAllWithComparator() {
         Project projectC = createModel();
         projectC.setName("C");
-        getUserOwnedRepository().add("1", projectC);
+        getUserOwnedRepository().add(UUID1, projectC);
 
         Project projectB = createModel();
         projectB.setName("B");
-        getUserOwnedRepository().add("2", projectB);
+        getUserOwnedRepository().add(UUID2, projectB);
 
         Project projectA = createModel();
         projectA.setName("A");
-        getUserOwnedRepository().add("2", projectA);
+        getUserOwnedRepository().add(UUID2, projectA);
 
         List<Project> sortedExpected = Arrays.asList(projectA, projectB);
-        List<Project> sortedActual = getUserOwnedRepository().findAll("2", Comparator.comparing(Project::getName));
+        List<Project> sortedActual = getUserOwnedRepository().findAll(UUID2, Comparator.comparing(Project::getName));
         Assert.assertEquals(sortedExpected, sortedActual);
         Assert.assertThrows(
                 NullPointerException.class,
@@ -96,18 +107,18 @@ public final class ProjectRepositoryTest extends AbstractUserOwnedRepositoryTest
     public void testFindAllWithSort() {
         Project projectC = createModel();
         projectC.setName("C");
-        getUserOwnedRepository().add("1", projectC);
+        getUserOwnedRepository().add(UUID1, projectC);
 
         Project projectB = createModel();
         projectB.setName("B");
-        getUserOwnedRepository().add("2", projectB);
+        getUserOwnedRepository().add(UUID2, projectB);
 
         Project projectA = createModel();
         projectA.setName("A");
-        getUserOwnedRepository().add("2", projectA);
+        getUserOwnedRepository().add(UUID2, projectA);
 
         List<Project> sortedExpected = Arrays.asList(projectA, projectB);
-        List<Project> sortedActual = getUserOwnedRepository().findAll("2", Sort.BY_NAME);
+        List<Project> sortedActual = getUserOwnedRepository().findAll(UUID2, Sort.BY_NAME);
         Assert.assertEquals(sortedExpected, sortedActual);
         Assert.assertThrows(
                 NullPointerException.class,
@@ -115,7 +126,7 @@ public final class ProjectRepositoryTest extends AbstractUserOwnedRepositoryTest
         );
         Assert.assertThrows(
                 NullPointerException.class,
-                () -> getUserOwnedRepository().findAll("2", (Sort) null)
+                () -> getUserOwnedRepository().findAll(UUID2, (Sort) null)
         );
     }
 
