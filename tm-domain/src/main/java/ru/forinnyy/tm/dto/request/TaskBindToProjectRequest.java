9a8b79cac79a1ef193b6de813ee9d7e01c497b91
Pavel Forinnyy <forinnyy@gmail.com>
2025-09-20 15:43:41 +0400

    Switch to database.

--- tm-domain/src/main/java/ru/forinnyy/tm/dto/request/TaskBindToProjectRequest.java
+++ tm-domain/src/main/java/ru/forinnyy/tm/dto/request/TaskBindToProjectRequest.java
@@ -13,8 +13,6 @@ public final class TaskBindToProjectRequest extends AbstractUserRequest {
 
     private String projectId;
 
-    private String token;
-
     public TaskBindToProjectRequest(String token) {
         super(token);
     }
