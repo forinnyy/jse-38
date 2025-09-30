9a80019a62477610fd4ce4e1c0c289412af0f884
Pavel Forinnyy <forinnyy@gmail.com>
2025-09-30 14:53:08 +0400

    Tests for server fix.

--- tm-server/src/test/java/ru/forinnyy/tm/service/ProjectTaskServiceTest.java
+++ tm-server/src/test/java/ru/forinnyy/tm/service/ProjectTaskServiceTest.java
@@ -1,160 +1,107 @@
-//package ru.forinnyy.tm.service;
-//
-//import lombok.SneakyThrows;
-//import org.junit.Assert;
-//import org.junit.Test;
-//import org.junit.experimental.categories.Category;
-//import ru.forinnyy.tm.AbstractTest;
-//import ru.forinnyy.tm.api.repository.IProjectRepository;
-//import ru.forinnyy.tm.api.repository.ITaskRepository;
-//import ru.forinnyy.tm.exception.entity.ProjectNotFoundException;
-//import ru.forinnyy.tm.exception.entity.TaskNotFoundException;
-//import ru.forinnyy.tm.exception.field.ProjectIdEmptyException;
-//import ru.forinnyy.tm.exception.field.TaskIdEmptyException;
-//import ru.forinnyy.tm.exception.field.UserIdEmptyException;
-//import ru.forinnyy.tm.marker.UnitCategory;
-//import ru.forinnyy.tm.model.Project;
-//import ru.forinnyy.tm.model.Task;
-//import ru.forinnyy.tm.repository.ProjectRepository;
-//import ru.forinnyy.tm.repository.TaskRepository;
-//
-//
-//@Category(UnitCategory.class)
-//public final class ProjectTaskServiceTest extends AbstractTest {
-//
-//    private final IProjectRepository projectRepository = new ProjectRepository();
-//    private final ITaskRepository taskRepository = new TaskRepository();
-//    private final ProjectTaskService projectTaskService = new ProjectTaskService(projectRepository, taskRepository);
-//
-//    @Test
-//    @SneakyThrows
-//    public void testBindTaskToProject() {
-//        Project project = new Project();
-//        project.setId(UUID2);
-//        project.setUserId(UUID1);
-//        projectRepository.add(project);
-//
-//        Project wrongProject = new Project();
-//        wrongProject.setId(UUID4);
-//        wrongProject.setUserId(UUID2);
-//        projectRepository.add(wrongProject);
-//
-//        Task task = new Task();
-//        task.setId(UUID3);
-//        task.setUserId(UUID1);
-//        taskRepository.add(task);
-//
-//        Task bindedTask = projectTaskService.bindTaskToProject(UUID1, UUID2, UUID3);
-//        Assert.assertNotNull(bindedTask);
-//        Assert.assertEquals(UUID2, bindedTask.getProjectId());
-//
-//        Assert.assertThrows(UserIdEmptyException.class,
-//                () -> projectTaskService.bindTaskToProject(null, UUID2, UUID3));
-//        Assert.assertThrows(UserIdEmptyException.class,
-//                () -> projectTaskService.bindTaskToProject(EMPTY_STRING, UUID2, UUID3));
-//
-//        Assert.assertThrows(ProjectIdEmptyException.class,
-//                () -> projectTaskService.bindTaskToProject(UUID1, null, UUID3));
-//        Assert.assertThrows(ProjectIdEmptyException.class,
-//                () -> projectTaskService.bindTaskToProject(UUID1, EMPTY_STRING, UUID3));
-//
-//        Assert.assertThrows(TaskIdEmptyException.class,
-//                () -> projectTaskService.bindTaskToProject(UUID1, UUID2, null));
-//        Assert.assertThrows(TaskIdEmptyException.class,
-//                () -> projectTaskService.bindTaskToProject(UUID1, UUID2, EMPTY_STRING));
-//
-//        Assert.assertThrows(ProjectNotFoundException.class,
-//                () -> projectTaskService.bindTaskToProject(UUID1, UUID4, UUID3));
-//        Assert.assertThrows(ProjectNotFoundException.class,
-//                () -> projectTaskService.bindTaskToProject(UUID1, UUID5, UUID3));
-//
-//        Assert.assertThrows(TaskNotFoundException.class,
-//                () -> projectTaskService.bindTaskToProject(UUID1, UUID2, UUID4));
-//
-//        // TODO PermissionException
-//    }
-//
-//    @Test
-//    @SneakyThrows
-//    public void testUnbindTaskFromProject() {
-//        Project project = new Project();
-//        project.setId(UUID2);
-//        project.setUserId(UUID1);
-//        projectRepository.add(project);
-//
-//        Task task = new Task();
-//        task.setId(UUID3);
-//        task.setUserId(UUID1);
-//        task.setProjectId(UUID2);
-//        taskRepository.add(task);
-//
-//        Task unbindedTask = projectTaskService.unbindTaskFromProject(UUID1, UUID2, UUID3);
-//        Assert.assertNotNull(unbindedTask);
-//        Assert.assertNull(unbindedTask.getProjectId());
-//
-//        Assert.assertThrows(UserIdEmptyException.class,
-//                () -> projectTaskService.unbindTaskFromProject(null, UUID2, UUID3));
-//        Assert.assertThrows(UserIdEmptyException.class,
-//                () -> projectTaskService.unbindTaskFromProject(EMPTY_STRING, UUID2, UUID3));
-//
-//        Assert.assertThrows(ProjectIdEmptyException.class,
-//                () -> projectTaskService.unbindTaskFromProject(UUID1, null, UUID3));
-//        Assert.assertThrows(ProjectIdEmptyException.class,
-//                () -> projectTaskService.unbindTaskFromProject(UUID1, EMPTY_STRING, UUID3));
-//
-//        Assert.assertThrows(TaskIdEmptyException.class,
-//                () -> projectTaskService.unbindTaskFromProject(UUID1, UUID2, null));
-//        Assert.assertThrows(TaskIdEmptyException.class,
-//                () -> projectTaskService.unbindTaskFromProject(UUID1, UUID2, EMPTY_STRING));
-//
-//        Assert.assertThrows(ProjectNotFoundException.class,
-//                () -> projectTaskService.unbindTaskFromProject(UUID1, UUID4, UUID3));
-//        Assert.assertThrows(ProjectNotFoundException.class,
-//                () -> projectTaskService.unbindTaskFromProject(UUID1, UUID5, UUID3));
-//
-//        Assert.assertThrows(TaskNotFoundException.class,
-//                () -> projectTaskService.unbindTaskFromProject(UUID1, UUID2, UUID4));
-//
-//        // TODO PermissionException
-//    }
-//
-//    @Test
-//    @SneakyThrows
-//    public void testRemoveProjectById() {
-//        Project project = new Project();
-//        project.setId(UUID2);
-//        project.setUserId(UUID1);
-//        projectRepository.add(project);
-//
-//        Task task = new Task();
-//        task.setId(UUID3);
-//        task.setUserId(UUID1);
-//        task.setProjectId(UUID2);
-//        taskRepository.add(task);
-//
-//        projectTaskService.removeProjectById(UUID1, UUID2);
-//
-//        Assert.assertNull(projectRepository.findOneById(UUID1, UUID2));
-//        Assert.assertTrue(taskRepository.findAllByProjectId(UUID1, UUID2).isEmpty());
-//
-//        Assert.assertThrows(UserIdEmptyException.class,
-//                () -> projectTaskService.removeProjectById(null, UUID2));
-//        Assert.assertThrows(UserIdEmptyException.class,
-//                () -> projectTaskService.removeProjectById(EMPTY_STRING, UUID2));
-//
-//        Assert.assertThrows(ProjectIdEmptyException.class,
-//                () -> projectTaskService.removeProjectById(UUID1, null));
-//        Assert.assertThrows(ProjectIdEmptyException.class,
-//                () -> projectTaskService.removeProjectById(UUID1, EMPTY_STRING));
-//
-//        Assert.assertThrows(ProjectNotFoundException.class,
-//                () -> projectTaskService.removeProjectById(UUID1, UUID3));
-//        Assert.assertThrows(ProjectNotFoundException.class,
-//                () -> projectTaskService.removeProjectById(UUID4, UUID2));
-//
-//        // TODO PermissionException
-//    }
-//
-//
-//
-//}
+package ru.forinnyy.tm.service;
+
+import lombok.NonNull;
+import lombok.SneakyThrows;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import ru.forinnyy.tm.AbstractTest;
+import ru.forinnyy.tm.api.service.IConnectionService;
+import ru.forinnyy.tm.exception.entity.ProjectNotFoundException;
+import ru.forinnyy.tm.exception.entity.TaskNotFoundException;
+import ru.forinnyy.tm.exception.field.ProjectIdEmptyException;
+import ru.forinnyy.tm.exception.field.TaskIdEmptyException;
+import ru.forinnyy.tm.exception.field.UserIdEmptyException;
+import ru.forinnyy.tm.marker.UnitCategory;
+import ru.forinnyy.tm.model.Project;
+import ru.forinnyy.tm.model.Task;
+
+@Category(UnitCategory.class)
+public final class ProjectTaskServiceTest extends AbstractTest {
+
+    private IConnectionService connectionService;
+
+    private ProjectService projectService;
+
+    private TaskService taskService;
+
+    private ProjectTaskService projectTaskService;
+
+    @Before
+    public void init() {
+        connectionService = new ConnectionService(new PropertyService());
+        projectService = new ProjectService(connectionService);
+        taskService = new TaskService(connectionService);
+        projectTaskService = new ProjectTaskService(connectionService);
+
+        projectService.initTable();
+        taskService.initTable();
+    }
+
+    @Test
+    @SneakyThrows
+    public void testBindTaskToProject() {
+        @NonNull final Project project = projectService.create(UUID1, "Project1", "Desc");
+        @NonNull final Task task = taskService.create(UUID1, "Task1", "Desc");
+
+        @NonNull final Task bindedTask = projectTaskService.bindTaskToProject(UUID1, project.getId(), task.getId());
+        Assert.assertNotNull(bindedTask);
+        Assert.assertEquals(project.getId(), bindedTask.getProjectId());
+
+        Assert.assertThrows(UserIdEmptyException.class,
+                () -> projectTaskService.bindTaskToProject(null, project.getId(), task.getId()));
+        Assert.assertThrows(ProjectIdEmptyException.class,
+                () -> projectTaskService.bindTaskToProject(UUID1, null, task.getId()));
+        Assert.assertThrows(TaskIdEmptyException.class,
+                () -> projectTaskService.bindTaskToProject(UUID1, project.getId(), null));
+        Assert.assertThrows(ProjectNotFoundException.class,
+                () -> projectTaskService.bindTaskToProject(UUID1, UUID5, task.getId()));
+        Assert.assertThrows(TaskNotFoundException.class,
+                () -> projectTaskService.bindTaskToProject(UUID1, project.getId(), UUID5));
+    }
+
+    @Test
+    @SneakyThrows
+    public void testUnbindTaskFromProject() {
+        @NonNull final Project project = projectService.create(UUID1, "Project1", "Desc");
+        @NonNull final Task task = taskService.create(UUID1, "Task1", "Desc");
+        projectTaskService.bindTaskToProject(UUID1, project.getId(), task.getId());
+
+        @NonNull final Task unbindedTask = projectTaskService.unbindTaskFromProject(UUID1, project.getId(), task.getId());
+        Assert.assertNotNull(unbindedTask);
+        Assert.assertNull(unbindedTask.getProjectId());
+
+        Assert.assertThrows(UserIdEmptyException.class,
+                () -> projectTaskService.unbindTaskFromProject(null, project.getId(), task.getId()));
+        Assert.assertThrows(ProjectIdEmptyException.class,
+                () -> projectTaskService.unbindTaskFromProject(UUID1, null, task.getId()));
+        Assert.assertThrows(TaskIdEmptyException.class,
+                () -> projectTaskService.unbindTaskFromProject(UUID1, project.getId(), null));
+        Assert.assertThrows(ProjectNotFoundException.class,
+                () -> projectTaskService.unbindTaskFromProject(UUID1, UUID5, task.getId()));
+        Assert.assertThrows(TaskNotFoundException.class,
+                () -> projectTaskService.unbindTaskFromProject(UUID1, project.getId(), UUID5));
+    }
+
+    @Test
+    @SneakyThrows
+    public void testRemoveProjectById() {
+        @NonNull final Project project = projectService.create(UUID1, "Project1", "Desc");
+        @NonNull final Task task = taskService.create(UUID1, "Task1", "Desc");
+        projectTaskService.bindTaskToProject(UUID1, project.getId(), task.getId());
+
+        projectTaskService.removeProjectById(UUID1, project.getId());
+
+        Assert.assertFalse(projectService.existsById(UUID1, project.getId()));
+        Assert.assertTrue(taskService.findAllByProjectId(UUID1, project.getId()).isEmpty());
+
+        Assert.assertThrows(UserIdEmptyException.class,
+                () -> projectTaskService.removeProjectById(null, project.getId()));
+        Assert.assertThrows(ProjectIdEmptyException.class,
+                () -> projectTaskService.removeProjectById(UUID1, null));
+        Assert.assertThrows(ProjectNotFoundException.class,
+                () -> projectTaskService.removeProjectById(UUID1, UUID5));
+    }
+
+}
